cd3d59ac799c6129274199146b77004c9a0ed9c4
JunnuD <junnudanhammer@gmail.com>
2024-01-09 13:24:08 +0200

    Update Hello.py

--- Ex.1/Hello.py
+++ Ex.1/Hello.py
@@ -3,3 +3,14 @@
 print()
 print("Eeeellouu Mate!")
 print()
+
+numerolist = [1,2,3,4,5,6,]
+mydictionary = {}
+
+number = int(input("Please give a number: "))
+numerolist.append(number)
+
+print("Length of the full list: ",len(numerolist))
+#print(numerolist)
+
+
