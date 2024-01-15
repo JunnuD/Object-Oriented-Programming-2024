105061e8b04b3e9e55a3e98cc979bef9148cd436
Junnu Danhammer <junnudanhammer@gmail.com>
2024-01-15 22:30:35 +0200

    Created the last assingment files and information added

---
+++ Ex.1/10.py
@@ -0,0 +1,12 @@
+# Write a phone book application. It should work as follows:
+# command (1 search, 2 add, 3 quit): 
+# 2name: peternumber: 040-5466745 ok!
+# command (1 search, 2 add, 3 quit): 2 name: emilynumber: 045-1212344 ok!
+# command (1 search, 2 add, 3 quit): 1name: peter 040-5466745 
+# command (1 search, 2 add, 3 quit): 1name: maryno number
+# command (1 search, 2 add, 3 quit): 2name: peternumber: 09-22223333ok!
+# command (1 search, 2 add, 3 quit): 1name: peter09-22223333
+# command (1 search, 2 add, 3 quit): 3 quitting...
+
+
+print("Hommihi!")
---
+++ Ex.1/9.py
@@ -0,0 +1,4 @@
+#Code a function that returns a random number between 1-6 when calling it. 
+# Print out the number where the function is called (so do not print the number inside the function). 
+# Name the function properly (see style guide).
+
