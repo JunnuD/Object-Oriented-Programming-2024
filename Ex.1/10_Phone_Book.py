d9a19dc805351f12e4556e277f81653bcb35f304
JunnuD <junnudanhammer@gmail.com>
2024-01-16 10:54:44 +0200

    Update 10_Phone_Book.py

--- Ex.1/10_Phone_Book.py
+++ Ex.1/10_Phone_Book.py
@@ -1,14 +1,31 @@
 # Write a phone book application. It should work as follows:
-# command (1 search, 2 add, 3 quit): 
-# 2name: peternumber: 040-5466745 ok!
+# command (1 search, 2 add, 3 quit): 2 name: peternumber: 040-5466745 ok!
 # command (1 search, 2 add, 3 quit): 2 name: emilynumber: 045-1212344 ok!
-# command (1 search, 2 add, 3 quit): 1name: peter 040-5466745 
-# command (1 search, 2 add, 3 quit): 1name: maryno number
-# command (1 search, 2 add, 3 quit): 2name: peternumber: 09-22223333ok!
-# command (1 search, 2 add, 3 quit): 1name: peter09-22223333
+# command (1 search, 2 add, 3 quit): 1 name: peter 040-5466745 
+# command (1 search, 2 add, 3 quit): 1 name: maryno number
+# command (1 search, 2 add, 3 quit): 2 name: peternumber: 09-22223333ok!
+# command (1 search, 2 add, 3 quit): 1 name: peter09-22223333
 # command (1 search, 2 add, 3 quit): 3 quitting...
 
 # Author: Junnu Danhammer
 # Description: Above!
 
 print("Hommihi!")
+
+
+def get_user_input():
+    choice = input("Choose one --> 1 : Search, 2 : Add, 3 : Quit ")
+
+def get_user_search():
+
+def get_user_add():
+
+def get_quit_program():
+
+def main():
+    list_of_people_in = {{"Name: Peter", "Number: 040-5466745"},{"Name: Emily", "Number: 045-1212344"}, 
+                        {"Name: Emily", "Number: 045-1212344"}}
+                        # ACTUALLY NO! I an supposed to import this into the program not have it already in there.
+    get_user_input()
+
+main()
