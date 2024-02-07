8434e9a907a2dba021d4a8f259fee381eb750b63
Junnu Danhammer <junnudanhammer@gmail.com>
2024-02-07 18:05:30 +0200

    Finished Ex1 part 1

--- Ex.3/2_statisctics.py
+++ Ex.3/2_statisctics.py
@@ -18,22 +18,47 @@ class NumberStats:
         if not self.numbers:
             return 0
         return sum(self.numbers) / len(self.numbers)
-
+    
+    def separate_sum(self): # Part 4
+        even_sum = sum(num for num in self.numbers if num % 2 == 0)
+        odd_sum = sum(num for num in self.numbers if num % 2 != 0)
+        return even_sum, odd_sum
+        
+        
 if __name__ == "__main__":
     # Part 1 test prints:
     stats = NumberStats()
-    stats.add_number(3)
+    """stats.add_number(3)
     stats.add_number(5)
     stats.add_number(1)
     stats.add_number(2)
-    print("Numbers added:", stats.count_numbers())
+    print("Numbers added:", stats.count_numbers())"""
 
     #Part 2 test prints:
     stats = NumberStats()
-    stats.add_number(3)
+    """stats.add_number(3)
     stats.add_number(5)
     stats.add_number(1)
     stats.add_number(2)
     print("Numbers added:", stats.count_numbers())
     print("Sum of numbers:", stats.get_sum())
-    print("Mean of numbers:", stats.average())
+    print("Mean of numbers:", stats.average())"""
+    
+
+while True:
+    user_input = input("Enter an number to the list (-1 to close): ")
+    if user_input == "-1":
+           break
+    try:
+        number = int(user_input)
+        stats.add_number(number)
+    except ValueError:
+            print("Invalid input. Please enter a valid integer. ")
+        
+print("Numbers added:", stats.count_numbers())
+print("Sum of numbers:", stats.get_sum())
+print("Mean of numbers:", stats.average())
+
+even_sum, odd_sum = stats.separate_sum()
+print("Sum of even numbers:", even_sum)
+print("Sum of odd numbers:", odd_sum)
