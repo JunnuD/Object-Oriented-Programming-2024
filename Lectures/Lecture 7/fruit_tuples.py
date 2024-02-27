829019d77444e2d1c11e88b3a2c98201ce78a83e
JunnuD <junnudanhammer@gmail.com>
2024-02-27 11:00:33 +0200

    Update fruit_tuples.py

--- Lectures/Lecture 7/fruit_tuples.py
+++ Lectures/Lecture 7/fruit_tuples.py
@@ -6,11 +6,38 @@ print ("Sort by the first item")
 for product in products:
     print(product)
 
+print()
 print("Order by the second item in tuple")
 
 def order_by_price(item: tuple):
-    return 
+    return item[1]
+
 
 products1 = [("banana", 5.95), ("apple", 3.95), ("orange", 4.50), ("watermelon", 4.95)]
 
 products1.sort(key=order_by_price)
+for product in products1:
+    print(product)
+
+
+print()
+print("Use sorted function")
+products2 = [("banana", 5.95), ("apple", 3.95), ("orange", 4.50), ("watermelon", 4.95)]
+t2 = sorted(products2, key=order_by_price)
+for i in t2:
+    print(i)
+
+
+print()
+for i in products2:
+    print(i)
+
+print()
+
+def sort_by_price(items: list):
+    return sorted(items, key=order_by_price)
+
+products3 = [("banana", 5.95), ("apple", 3.95), ("orange", 4.50), ("watermelon", 4.95)]
+
+for product in sort_by_price(products3):
+    print(product)
