eef2538e6b2386f84649f6e5baa14ef16986515e
JunnuD <junnudanhammer@gmail.com>
2024-02-27 11:43:00 +0200

    Update lambda_example.py

--- Lectures/Lecture 7/lambda_example.py
+++ Lectures/Lecture 7/lambda_example.py
@@ -10,3 +10,21 @@ for word in sorted(strings, key=lambda word: "".join([c for c in word if c in "a
     lambda expression
     lambda <parameters>: <expression>
     """ 
+
+
+print()
+print("Old Fashioned way")
+
+new_list = []
+for word in strings:
+    for c in word:
+        if c in "aeiou":
+            find = True
+    if find:
+        new_list.append(word)
+    find = False
+
+sorted_list = sorted(new_list)
+
+for word in sorted_list:
+    print(word)
