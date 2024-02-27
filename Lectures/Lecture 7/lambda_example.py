7094068a2d3412f91d2a3f71ab7deb2f358eb845
JunnuD <junnudanhammer@gmail.com>
2024-02-27 11:38:14 +0200

    Update lambda_example.py

--- Lectures/Lecture 7/lambda_example.py
+++ Lectures/Lecture 7/lambda_example.py
@@ -2,3 +2,11 @@ strings = ["Mickey","Mack", "Marvin", "Mikke", "Merlin", "Mck"]
 
 for word in sorted(strings, key=lambda word: "".join([c for c in word if c in "aeiou"])):
     print(word)
+
+    """
+    List comprehension
+    [<expression> for <item> in <series>]
+
+    lambda expression
+    lambda <parameters>: <expression>
+    """ 
