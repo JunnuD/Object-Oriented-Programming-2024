e473518834657950c1f0ba2bfe426473d66d1e32
JunnuD <junnudanhammer@gmail.com>
2024-02-27 11:23:26 +0200

    Update students_dictionary.py

---
+++ Lectures/Lecture 7/students_dictionary.py
@@ -0,0 +1,9 @@
+
+if __name__== "__main__":
+    students_data = [
+    {"Name": "Marvin","id": "M321", "Credits": 220},
+    {"Name": "Anna", "id": "A999", "Credits": 220},
+    {"Name":"Eric", "id": "O453", "Credits": 120},
+    {"Name":"Ben", "id": "L982", "Credits": 235},
+    {"Name":"Archie", "id": "A123", "Credits": 220}
+    ]
